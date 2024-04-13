6e8c2591914b37d6e9803ecb2a17eee24d50e8eb
ashtkarim <assihoutkarim@gmail.com>
2024-04-13 21:10:39 +0000

    fix my code 2

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
+"""Class Square."""
 
-class square():
 
+class Square():
+    """Modules square."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        Loop through the keyword arguments
+        and set the corresponding attribute
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.height * self.width
+        """Area of the Square."""
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """Permiter of the Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Returns a string representation of the square.
+        The format is "width/height".
+        """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
